bbe5851adf605c04df225f22d18d9248d25f3954
notbendreyfuss <47282313+notbendreyfuss@users.noreply.github.com>
2020-08-29 02:19:41 -0400

    Added 86 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19909,
-	"created_at": "Fri Aug 28 06:19:38 +0000 2020",
+	"tweets": 19995,
+	"created_at": "Sat Aug 29 06:19:40 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_08.js",
 		"year": 2020,
 		"var_name": "tweets_2020_08",
-		"tweet_count": 1781,
+		"tweet_count": 1867,
 		"month": 8
 	},
 	{
