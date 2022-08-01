b82a4aa65d2980894c2d36ef1e72b18896998ab4
notbendreyfuss <47282313+notbendreyfuss@users.noreply.github.com>
2022-08-01 02:19:25 -0400

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 54801,
-	"created_at": "Sun Jul 31 06:19:33 +0000 2022",
+	"tweets": 54804,
+	"created_at": "Mon Aug 01 06:19:24 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_08.js
@@ -0,0 +1,507 @@
+Grailbird.data.tweets_2022_08 = 
+[
+	{
+		"created_at": "Mon Aug 01 06:19:05 +0000 2022",
+		"id": 1553988668829749200,
+		"id_str": "1553988668829749248",
+		"full_text": "Imagine being elected prime minister of Australia and this is the piece of shit car they give you https://t.co/n3hiPxRlv1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1553988664417403000,
+					"id_str": "1553988664417402881",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FZDgdPeVQAEqB8c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZDgdPeVQAEqB8c.jpg",
+					"url": "https://t.co/n3hiPxRlv1",
+					"display_url": "pic.twitter.com/n3hiPxRlv1",
+					"expanded_url": "https://twitter.com/bendreyfuss/status/1553988668829749248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1553988664417403000,
+					"id_str": "1553988664417402881",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FZDgdPeVQAEqB8c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZDgdPeVQAEqB8c.jpg",
+					"url": "https://t.co/n3hiPxRlv1",
+					"display_url": "pic.twitter.com/n3hiPxRlv1",
+					"expanded_url": "https://twitter.com/bendreyfuss/status/1553988668829749248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18269124,
+			"id_str": "18269124",
+			"name": "Ben Dreyfuss",
+			"screen_name": "bendreyfuss",
+			"location": "United States",
+			"description": "calm down",
+			"url": "https://t.co/ZNogBKfj3K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZNogBKfj3K",
+							"expanded_url": "http://bendreyfuss.substack.com",
+							"display_url": "bendreyfuss.substack.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 79778,
+			"friends_count": 916,
+			"listed_count": 982,
+			"created_at": "Sat Dec 20 17:19:32 +0000 2008",
+			"favourites_count": 118702,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1398,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1448781155617030147/OZPBnb59_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1448781155617030147/OZPBnb59_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18269124/1620155559",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Imagine being elected prime minister of Australia and this is the piece of shit car they give you https://t.co/n3hiPxRlv1"
+	},
+	{
+		"created_at": "Mon Aug 01 04:49:07 +0000 2022",
+		"id": 1234821531777323000,
+		"id_str": "1553966028169760771",
+		"full_text": "Too late @SNICKERSUK https://t.co/vCjmUCH0Az",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SNICKERSUK",
+					"name": "SNICKERS UK",
+					"id": 440949016,
+					"id_str": "440949016",
+					"indices": [
+						9,
+						20
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1234821253980094500,
+					"id_str": "1234821253980094467",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/ESL3Y1yWAAMwhOT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESL3Y1yWAAMwhOT.jpg",
+					"url": "https://t.co/vCjmUCH0Az",
+					"display_url": "pic.twitter.com/vCjmUCH0Az",
+					"expanded_url": "https://twitter.com/GPJEWELL/status/1234821531777323015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1234821253980094500,
+					"id_str": "1234821253980094467",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/ESL3Y1yWAAMwhOT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESL3Y1yWAAMwhOT.jpg",
+					"url": "https://t.co/vCjmUCH0Az",
+					"display_url": "pic.twitter.com/vCjmUCH0Az",
+					"expanded_url": "https://twitter.com/GPJEWELL/status/1234821531777323015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1234821253980205000,
+					"id_str": "1234821253980205056",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/ESL3Y1yXsAA7bDD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESL3Y1yXsAA7bDD.jpg",
+					"url": "https://t.co/vCjmUCH0Az",
+					"display_url": "pic.twitter.com/vCjmUCH0Az",
+					"expanded_url": "https://twitter.com/GPJEWELL/status/1234821531777323015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 242309035,
+			"id_str": "242309035",
+			"name": "Gareth Jewell",
+			"screen_name": "GPJEWELL",
+			"location": "🏴󠁧󠁢󠁷󠁬󠁳󠁿 Cymru",
+			"description": "la la how the life goes on...",
+			"url": "https://t.co/a6wzxzC8EO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/a6wzxzC8EO",
+							"expanded_url": "https://www.ylolfa.com/cynnyrch/9781784619534",
+							"display_url": "ylolfa.com/cynnyrch/97817…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2776,
+			"friends_count": 3319,
+			"listed_count": 6,
+			"created_at": "Mon Jan 24 13:39:16 +0000 2011",
+			"favourites_count": 7983,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6902,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1443687630021988354/F9gDiUBE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1443687630021988354/F9gDiUBE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/242309035/1601995302",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 30,
+		"favorite_count": 185,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Too late @SNICKERSUK https://t.co/vCjmUCH0Az"
+	},
+	{
+		"created_at": "Mon Aug 01 03:56:55 +0000 2022",
+		"id": 1553952895447421000,
+		"id_str": "1553952895447420928",
+		"full_text": "@Serfs_Road Karen is a girl’s name and if you are a trans man you should change your name to Keith or something",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			111
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Serfs_Road",
+					"name": "Hi Eck",
+					"id": 820477452,
+					"id_str": "820477452",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1553952604358582300,
+		"in_reply_to_status_id_str": "1553952604358582273",
+		"in_reply_to_user_id": 820477452,
+		"in_reply_to_user_id_str": "820477452",
+		"in_reply_to_screen_name": "Serfs_Road",
+		"user": {
+			"id": 18269124,
+			"id_str": "18269124",
+			"name": "Ben Dreyfuss",
+			"screen_name": "bendreyfuss",
+			"location": "United States",
+			"description": "calm down",
+			"url": "https://t.co/ZNogBKfj3K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZNogBKfj3K",
+							"expanded_url": "http://bendreyfuss.substack.com",
+							"display_url": "bendreyfuss.substack.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 79778,
+			"friends_count": 916,
+			"listed_count": 982,
+			"created_at": "Sat Dec 20 17:19:32 +0000 2008",
+			"favourites_count": 118702,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1398,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1448781155617030147/OZPBnb59_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1448781155617030147/OZPBnb59_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18269124/1620155559",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Serfs_Road Karen is a girl’s name and if you are a trans man you should change your name to Keith or something"
+	}
+]
